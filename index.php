9790863d1efd769403c23d2ff9fa26e575961995
Levi Lindsey <levisl176@gmail.com>
2013-09-05 14:31:57 -0700

    tweaked layout

--- index.php
+++ index.php
@@ -67,157 +67,168 @@
 		</div>
 
 		<div id="infoArea">
-			<h2>Directions</h2>
-			<table id="directionsTable">
+			<div id="directionsArea">
+				<h2>Directions</h2>
+				<table id="directionsTable">
+					<p>
+						Just, like, be awesome.  And win.
+					</p>
+					<tr>
+						<td class="directionPanel">
+							<h3>Rotate Block</h3>
+							<img src="img/directions_rotate.png" alt="Rotate block">
+							<p>Tap on block.<p>
+						</td>
+						<td class="directionPanel">
+							<h3>Move Block Sideways</h3>
+							<img src="img/directions_move.png" alt="Move block sideways">
+							<p>Press on block, and drag sideways.<p>
+						</td>
+					</tr>
+					<tr>
+						<td class="directionPanel">
+							<h3>Drop Block</h3>
+							<img src="img/directions_drop.png" alt="Drop block">
+							<p>Press on block, and drag down (toward fall direction).<p>
+						</td>
+						<td class="directionPanel">
+							<h3>Switch Block Fall Direction</h3>
+							<img src="img/directions_switch_fall_direction.png" alt="Switch block fall direction">
+							<p>Press on block, and drag up (away from current fall direction).<p>
+						</td>
+					</tr>
+				</table>
 				<p>
-					Just, like, be awesome.  And win.
+					Press the spacebar to pause.
 				</p>
-				<tr>
-					<td class="directionPanel">
-						<h3>Rotate Block</h3>
-						<img src="img/directions_rotate.png" alt="Rotate block">
-						<p>Tap on block.<p>
-					</td>
-					<td class="directionPanel">
-						<h3>Move Block Sideways</h3>
-						<img src="img/directions_move.png" alt="Move block sideways">
-						<p>Press on block, and drag sideways.<p>
-					</td>
-				</tr>
-				<tr>
-					<td class="directionPanel">
-						<h3>Drop Block</h3>
-						<img src="img/directions_drop.png" alt="Drop block">
-						<p>Press on block, and drag down (toward fall direction).<p>
-					</td>
-					<td class="directionPanel">
-						<h3>Switch Block Fall Direction</h3>
-						<img src="img/directions_switch_fall_direction.png" alt="Switch block fall direction">
-						<p>Press on block, and drag up (away from current fall direction).<p>
-					</td>
-				</tr>
-			</table>
-			<p>
-				Press the spacebar to pause.
-			</p>
+			</div>
 
 			<hr>
 
-			<h2>Options</h2>
-			<p>
-				<label><input type="checkbox" name="mode1" id="mode1" value="mode1"> Enable keyboard control</label>
-				<br>
-				<label><input type="checkbox" name="mode2" id="mode2" value="mode2"> Completing squares instead of lines</label>
-				<br>
-				<label><input type="checkbox" name="mode3" id="mode3" value="mode3" checked> Blocks are allowed to fall past the center</label>
-				<br>
-				<label><input type="checkbox" name="mode4" id="mode4" value="mode4" checked> Able to change falling direction of blocks</label>
-				<br>
-				<label><input type="checkbox" name="mode5" id="mode5" value="mode5" checked> Changing direction moves block to next quadrant</label>
-				<br>
-				<label><input type="checkbox" name="mode6" id="mode6" value="mode6" checked> Collapsing a layer causes higher layers to &quot;settle&quot;</label>
-				<br>
-				<label><input type="checkbox" name="mode7" id="mode7" value="mode7" checked> Bombs on</label>
-				<br>
-				Size of game area:
-				<select id="gameWindowSize">
-					<option value="10">10</option>
-					<option value="20">20</option>
-					<option value="40" selected>40</option>
-					<option value="60">60</option>
-					<option value="80">80</option>
-					<option value="100">100</option>
-					<option value="120">120</option>
-					<option value="140">140</option>
-					<option value="160">160</option>
-					<option value="180">180</option>
-					<option value="200">200</option>
-				</select>
-				<br>
-				Size of center square:
-				<select id="centerSquareSize">
-					<option value="2">2</option>
-					<option value="4">4</option>
-					<option value="6" selected>6</option>
-					<option value="8">8</option>
-					<option value="12">12</option>
-					<option value="14">14</option>
-					<option value="16">16</option>
-					<option value="18">18</option>
-					<option value="20">20</option>
-				</select>
-				<br>
-				Starting level:
-				<select id="startingLevel">
-					<option value="1" selected>1</option>
-					<option value="2">2</option>
-					<option value="3">3</option>
-					<option value="4">4</option>
-					<option value="5">5</option>
-					<option value="6">6</option>
-					<option value="7">7</option>
-					<option value="8">8</option>
-					<option value="9">9</option>
-					<option value="10">10</option>
-					<option value="15">15</option>
-					<option value="20">20</option>
-				</select>
-				<br>
-				<label><input type="checkbox" name="showConsole" id="showConsole" value="showConsole"> Show the debug console</label>
-			</p>
+			<div id="optionsArea">
+				<h2>Options</h2>
+				<p>
+					<label><input type="checkbox" name="mode1" id="mode1" value="mode1"> Enable keyboard control</label>
+					<br>
+					<label><input type="checkbox" name="mode2" id="mode2" value="mode2"> Completing squares instead of lines</label>
+					<br>
+					<label><input type="checkbox" name="mode3" id="mode3" value="mode3" checked> Blocks are allowed to fall past the center</label>
+					<br>
+					<label><input type="checkbox" name="mode4" id="mode4" value="mode4" checked> Able to change falling direction of blocks</label>
+					<br>
+					<label><input type="checkbox" name="mode5" id="mode5" value="mode5" checked> Changing direction moves block to next quadrant</label>
+					<br>
+					<label><input type="checkbox" name="mode6" id="mode6" value="mode6" checked> Collapsing a layer causes higher layers to &quot;settle&quot;</label>
+					<br>
+					<label><input type="checkbox" name="mode7" id="mode7" value="mode7" checked> Bombs on</label>
+					<br>
+					Size of game area:
+					<select id="gameWindowSize">
+						<option value="10">10</option>
+						<option value="20">20</option>
+						<option value="40" selected>40</option>
+						<option value="60">60</option>
+						<option value="80">80</option>
+						<option value="100">100</option>
+						<option value="120">120</option>
+						<option value="140">140</option>
+						<option value="160">160</option>
+						<option value="180">180</option>
+						<option value="200">200</option>
+					</select>
+					<br>
+					Size of center square:
+					<select id="centerSquareSize">
+						<option value="2">2</option>
+						<option value="4">4</option>
+						<option value="6" selected>6</option>
+						<option value="8">8</option>
+						<option value="12">12</option>
+						<option value="14">14</option>
+						<option value="16">16</option>
+						<option value="18">18</option>
+						<option value="20">20</option>
+					</select>
+					<br>
+					Starting level:
+					<select id="startingLevel">
+						<option value="1" selected>1</option>
+						<option value="2">2</option>
+						<option value="3">3</option>
+						<option value="4">4</option>
+						<option value="5">5</option>
+						<option value="6">6</option>
+						<option value="7">7</option>
+						<option value="8">8</option>
+						<option value="9">9</option>
+						<option value="10">10</option>
+						<option value="15">15</option>
+						<option value="20">20</option>
+					</select>
+					<br>
+					<label><input type="checkbox" name="showConsole" id="showConsole" value="showConsole"> Show the debug console</label>
+				</p>
+			</div>
 
 			<hr>
 
-			<h2>Music</h2>
-			<p>
-				<label><input type="checkbox" name="aNightOfDizzySpells" id="aNightOfDizzySpells" value="aNightOfDizzySpells" checked> A Night Of Dizzy Spells</label>
+			<div id="musicArea">
+				<h2>Music</h2>
+				<div class="textColumn">
+					<label><input type="checkbox" name="aNightOfDizzySpells" id="aNightOfDizzySpells" value="aNightOfDizzySpells" checked> A Night Of Dizzy Spells</label>
+					<br>
+					<label><input type="checkbox" name="allOfUs" id="allOfUs" value="allOfUs" checked> All of Us</label>
+					<br>
+					<label><input type="checkbox" name="arpanauts" id="arpanauts" value="arpanauts" checked> Arpanauts</label>
+					<br>
+					<label><input type="checkbox" name="ascending" id="ascending" value="ascending" checked> Ascending</label>
+					<br>
+					<label><input type="checkbox" name="chibiNinja" id="chibiNinja" value="chibiNinja" checked> Chibi Ninja</label>
+					<br>
+					<label><input type="checkbox" name="comeAndFindMeB" id="comeAndFindMeB" value="comeAndFindMeB"> Come and Find Me - B mix</label>
+					<br>
+					<label><input type="checkbox" name="comeAndFindMe" id="comeAndFindMe" value="comeAndFindMe"> Come and Find Me</label>
+					<br>
+					<label><input type="checkbox" name="digitalNative" id="digitalNative" value="digitalNative" checked> Digital Native</label>
+				</div>
+				<div class="textColumn">
+					<label><input type="checkbox" name="hhavokIntro" id="hhavokIntro" value="hhavokIntro"> HHavok-intro</label>
+					<br>
+					<label><input type="checkbox" name="hhavokMain" id="hhavokMain" value="hhavokMain" checked> HHavok-main</label>
+					<br>
+					<label><input type="checkbox" name="jumpshot" id="jumpshot" value="jumpshot" checked> Jumpshot</label>
+					<br>
+					<label><input type="checkbox" name="prologue" id="prologue" value="prologue"> Prologue</label>
+					<br>
+					<label><input type="checkbox" name="searching" id="searching" value="searching" checked> Searching</label>
+					<br>
+					<label><input type="checkbox" name="underclocked" id="underclocked" value="underclocked" checked> Underclocked (underunderclocked mix)</label>
+					<br>
+					<label><input type="checkbox" name="wereAllUnderTheStars" id="wereAllUnderTheStars" value="wereAllUnderTheStars"> We're All Under the Stars</label>
+					<br>
+					<label><input type="checkbox" name="wereTheResistors" id="wereTheResistors" value="wereTheResistors" checked> We're the Resistors</label>
+				</div>
 				<br>
-				<label><input type="checkbox" name="allOfUs" id="allOfUs" value="allOfUs" checked> All of Us</label>
-				<br>
-				<label><input type="checkbox" name="arpanauts" id="arpanauts" value="arpanauts" checked> Arpanauts</label>
-				<br>
-				<label><input type="checkbox" name="ascending" id="ascending" value="ascending" checked> Ascending</label>
-				<br>
-				<label><input type="checkbox" name="chibiNinja" id="chibiNinja" value="chibiNinja" checked> Chibi Ninja</label>
-				<br>
-				<label><input type="checkbox" name="comeAndFindMeB" id="comeAndFindMeB" value="comeAndFindMeB" checked> Come and Find Me - B mix</label>
-				<br>
-				<label><input type="checkbox" name="comeAndFindMe" id="comeAndFindMe" value="comeAndFindMe" checked> Come and Find Me</label>
-				<br>
-				<label><input type="checkbox" name="digitalNative" id="digitalNative" value="digitalNative" checked> Digital Native</label>
-				<br>
-				<label><input type="checkbox" name="hhavokIntro" id="hhavokIntro" value="hhavokIntro" checked> HHavok-intro</label>
-				<br>
-				<label><input type="checkbox" name="hhavokMain" id="hhavokMain" value="hhavokMain" checked> HHavok-main</label>
-				<br>
-				<label><input type="checkbox" name="jumpshot" id="jumpshot" value="jumpshot" checked> Jumpshot</label>
-				<br>
-				<label><input type="checkbox" name="prologue" id="prologue" value="prologue" checked> Prologue</label>
-				<br>
-				<label><input type="checkbox" name="searching" id="searching" value="searching" checked> Searching</label>
-				<br>
-				<label><input type="checkbox" name="underclocked" id="underclocked" value="underclocked" checked> Underclocked (underunderclocked mix)</label>
-				<br>
-				<label><input type="checkbox" name="wereAllUnderTheStars" id="wereAllUnderTheStars" value="wereAllUnderTheStars" checked> We're All Under the Stars</label>
-				<br>
-				<label><input type="checkbox" name="wereTheResistors" id="wereTheResistors" value="wereTheResistors" checked> We're the Resistors</label>
-				<br>
-				All of these pieces are by 
-				<a href="http://ericskiff.com/music/" target="_blank">Eric Skiff</a>.
-			</p>
+				<p>
+					All of these pieces are by 
+					<a href="http://ericskiff.com/music/" target="_blank">Eric Skiff</a>.
+				</p>
+			</div>
 
 			<hr>
 
-			<h2>About</h2>
-			<p>
-				This game was made by <a href="www.jackieandlevi.com/levi.php">Levi Lindsey</a>.
-				<br><br>
-				The code is open source, and you can find it on GitHub at 
-				<a href="https://github.com/levisl176/squared_away" target="_blank">
-				https://github.com/levisl176/squared_away</a>.
-				<br><br>
-				Also, this app is coming soon to the Android marketplace.
-			</p>
+			<div id="aboutArea">
+				<h2>About</h2>
+				<p>
+					This game was made by <a href="www.jackieandlevi.com/levi.php">Levi Lindsey</a>.
+					<br><br>
+					The code is open source, and you can find it on GitHub at 
+					<a href="https://github.com/levisl176/squared_away" target="_blank">
+					https://github.com/levisl176/squared_away</a>.
+					<br><br>
+					Also, this app is coming soon to the Android marketplace.
+				</p>
+			</div>
 		</div>
 
 		<div id="console">
