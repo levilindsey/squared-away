ba1a2a51be2a023a89a95a91b750b9e9f7694bf4
Levi Lindsey <levisl176@gmail.com>
2013-09-20 16:38:23 -0700

    IN PROGRESS.  more file re-structuring.

--- app/squaredaway.js
+++ app/squaredaway.js
@@ -8,6 +8,7 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var config = require('./config')();
 
 var app = express();
 
@@ -32,6 +33,6 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(config.port, function(){
+  console.log('Express server listening on port ' + config.port);
 });
